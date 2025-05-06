0debf1d84de24f302193a9a422f5293c03901eda
Jan <jan.horcicka@email.cz>
2025-05-06 09:57:39 +0200

    zaklad

---
+++ webapp/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+#1 Model Trip linked with Customer and Guide models
+class Trip(models.Model):
+    trip_id = models.AutoField(primary_key=True)
+    trip_name = models.CharField(max_length=256)
+    trip_description = models.TextField(null=True, blank=True)
+    start_time = models.DateTimeField(null=True, blank=True)
+    end_time = models.DateTimeField(null=True, blank=True)
+    price = models.IntegerField(null=True, blank=True)
+    customers = models.ManyToManyField('Customer')
+    guides = models.ManyToManyField('Guide')
+
+    def __str__(self):
+        return self.trip_name
+
+# 2 Model Guide linked with Trip model
+class Guide(models.Model):
+    guide_id = models.AutoField(primary_key=True)
+    guide_name = models.CharField(max_length=256)
+    guided_trips = models.ManyToManyField('Trip')
+
+# 3 Model Customer linked with Trip model to easily determine who is going where
+class Customer(models.Model):
+    customer_id = models.AutoField(primary_key=True)
+    customer_first_name = models.CharField(max_length=64, null=True, blank=True)
+    customer_last_name = models.CharField(max_length=64, null=True, blank=True)
+    customer_email = models.EmailField(null=True, blank=True)
+    customer_phone = models.IntegerField(null=True, blank=True)
+    customer_trips = models.ManyToManyField('Trip')
+
---
+++ webapp/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
+
+#----------------
+#LIST VIEWS
+#----------------
+
+class CustomerListBaseView(TemplateView):
+    template_name = "webapp/customer_list_base.html"
+
+
+
+
+#----------------
+#CREATE VIEWS
+#----------------
+
+
+
+#----------------
+#UPDATE VIEWS
+#----------------
+
+
+
+#----------------
+#DELETE VIEWS
+#----------------
+
+
